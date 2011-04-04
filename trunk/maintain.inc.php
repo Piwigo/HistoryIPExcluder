2073b01cca01ff63e7f18263bcfe71ad70922064
Eric <Eric@piwigo.org>
2011-04-04 17:05:42 +0000

    Improved update mechanism. When no structural update of database is necessary, it sets the correct version number in plugin's configuration
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@10045 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/maintain.inc.php
+++ trunk/maintain.inc.php
@@ -59,6 +59,8 @@ function plugin_activate()
 {
   global $conf;
   
+  include_once (HIPE_PATH.'include/dbupgrade.inc.php');
+  
 /* Check for upgrade from 2.0.0 to 2.0.1 */
 /* *************************************** */
 	$query = '
@@ -97,13 +99,9 @@ WHERE param = "HistoryIPConfig"
     upgrade_211();
   }
 
-  /* upgrade from version 2.2.0 to 2.2.1 */
-  /* *********************************** */
-  $HIPE_Config = unserialize($conf['HistoryIPConfig']);
-  if ($HIPE_Config['Version'] == '2.2.0')
-  {
-    upgrade_220();
-  }
+  /* Global version number upgrade */
+  /* ***************************** */
+  global_version_update();
 }
 
 
@@ -129,128 +127,5 @@ WHERE param="HistoryIPConfig" LIMIT 1;
 
     pwg_query($q);
   }  
-}
-
-
-function upgrade_200()
-{
-  global $conf;
-  
-  $q = '
-UPDATE '.CONFIG_TABLE.'
-SET param = "HistoryIPExcluder"
-WHERE param = "nbc_HistoryIPExcluder"
-;';
-  pwg_query($q);
-
-  $q = '
-UPDATE '.CONFIG_TABLE.'
-SET comment = "History IP Excluder parameters"
-WHERE comment = "Parametres nbc History IP Excluder"
-;';
-  pwg_query($q);
-
-  upgrade_210();
-}
-
-function upgrade_210()
-{
-  global $conf;
-  
-  $default = array (
-    'Blacklist' => "0",
-    'Version'=> "2.1.1",
-  );
-
-  $q = '
-INSERT INTO '.CONFIG_TABLE.' (param,value,comment)
-VALUES ("HistoryIPConfig","'.addslashes(serialize($default)).'","History IP Excluder options");
-';
-      
-  pwg_query($q);
-}
-
-function upgrade_211()
-{
-  global $conf;
-
-// Update plugin version
-  $query = '
-SELECT value
-  FROM '.CONFIG_TABLE.'
-WHERE param = "HistoryIPConfig"
-;';
-  $result = pwg_query($query);
-  
-  $conf_HIPE = pwg_db_fetch_assoc($result);
-    
-  $Newconf_HIPE = unserialize($conf_HIPE['value']);
-  
-  $Newconf_HIPE['Version'] = '2.2.0';
-  
-  $update_conf = serialize($Newconf_HIPE);
-
-  $query = '
-UPDATE '.CONFIG_TABLE.'
-SET value="'.addslashes($update_conf).'"
-WHERE param="HistoryIPConfig"
-LIMIT 1
-;';
-
-	pwg_query($query);
-
-  // Create new HIPE entry in plugins table 
-  $query = '
-INSERT INTO '.PLUGINS_TABLE.' (id, state, version)
-VALUES ("HistoryIPExcluder","active","2.2.0")
-;';
-  
-  pwg_query($query);
-
-  // Delete old plugin entry in plugins table 
-  $query = '
-DELETE FROM '.PLUGINS_TABLE.'
-WHERE id="nbc_HistoryIPExcluder"
-LIMIT 1
-;';
-  
-  pwg_query($query);
-
-  // rename directory
-  if (!rename(PHPWG_PLUGINS_PATH.'nbc_HistoryIPExcluder', PHPWG_PLUGINS_PATH.'HistoryIPExcluder'))
-  {
-    die('Fatal error on plugin upgrade process : Unable to rename directory ! Please, rename manualy the plugin directory name from ../plugins/nbc_HistoryIPExcluder to ../plugins/HistoryIPExcluder.');
-  }
-}
-
-
-function upgrade_220()
-{
-  global $conf;
-
-// Update plugin version
-  $query = '
-SELECT value
-  FROM '.CONFIG_TABLE.'
-WHERE param = "HistoryIPConfig"
-;';
-  $result = pwg_query($query);
-  
-  $conf_HIPE = pwg_db_fetch_assoc($result);
-    
-  $Newconf_HIPE = unserialize($conf_HIPE['value']);
-  
-  $Newconf_HIPE['Version'] = '2.2.1';
-  
-  $update_conf = serialize($Newconf_HIPE);
-
-  $query = '
-UPDATE '.CONFIG_TABLE.'
-SET value="'.addslashes($update_conf).'"
-WHERE param="HistoryIPConfig"
-LIMIT 1
-;';
-
-	pwg_query($query);
 }
 ?>

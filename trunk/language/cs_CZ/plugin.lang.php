028a4898110f0068cb4ca8d43b91f972d5bdd6bd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-02 20:16:43 +0000

    [history_ip_excluder] Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@14671 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/cs_CZ/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_CleanHist'] = 'Vyčistit historii';
+$lang['HIPE_IPBlacklist_title'] = 'Černý list registrace';
+$lang['HIPE_IPByMember_description'] = 'Ukázat IP použité členy, řazené dle IP';
+$lang['HIPE_IPByMember'] = 'IP dle člena';
+$lang['HIPE_IPForMember_description'] = 'Hledat a ukázat IP spojené s registrovsným uživatelem (řazeno dle IP)';
+$lang['HIPE_IPForMember'] = 'IP pro člena';
+$lang['HIPE_MemberForIp'] = 'Členové pro jednu IP';
+$lang['HIPE_OnlyGuest'] = 'Pouze IP hostů';
+$lang['HIPE_admin_section1'] = 'Vyloučení IP';
+$lang['HIPE_admin_section2'] = 'Dotazy na tabulku historie';
+$lang['HIPE_admin_section3'] = 'Výsledek historické žádosti';
+$lang['HIPE_hist_cleaned'] = 'Vyčištění tabulky historie provedeno.';
+$lang['HIPE_resquet_ok'] = 'Žádost OK';
+$lang['HIPE_save_config'] = 'Konfigurace uložena.';
+$lang['HIPE_version'] = '- verze:';
+$lang['IP_geolocalisation'] = 'Geolokalizace';
+?>

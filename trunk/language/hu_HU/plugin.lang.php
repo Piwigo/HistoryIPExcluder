2df03bcfab9f9cabc9b3f48e41811ec253bea438
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-16 18:23:41 +0000

    [history_ip_excluder] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@15786 68402e56-0260-453c-a942-63ccdbb3a9ee

--- trunk/language/hu_HU/plugin.lang.php
+++ trunk/language/hu_HU/plugin.lang.php
@@ -4,7 +4,6 @@ $lang['HIPE_admin_section1'] = 'IP-k kizárása';
 $lang['HIPE_admin_description1'] = 'IP címek kizárásához írja be az alábbi mezőbe a kizárandó IP-ket, vagy adjon meg IP tartományokat (soronként egyet). IP tartomány kizárásához használja a helyettesítő karaktert "%".<br>Példa : 74.6.1.2 vagy 74.6.%';
 $lang['HIPE_save_config']='Beállítások mentve.';
 $lang['HIPE_CleanHist']='Előzmények törlése';
-$lang['submit']='Elküld';
 $lang['HIPE_admin_section2'] = 'Előzmények tábla lekérdezése';
 $lang['HIPE_admin_section3'] = 'Előzmények lekérdezésének eredménye';
 $lang['HIPE_IPByMember'] = 'Felhasználói IP-k';
@@ -31,6 +30,6 @@ $lang['HIPE_version'] = ' - Változat: ';
 // --------- Starting below: New or revised $lang ---- from version 2.1.1
 $lang['HIPE_IPBlacklist_title'] = 'Regisztrációs feketelista';
 $lang['HIPE_IPBlacklisted'] = ' Regisztráció tiltása az alábbi IP címekről (feketelista)';
-$lang['Error_HIPE_BlacklistedIP'] = 'Hiba! Az IP címed le van tiltva, nem regisztrálhatsz a galériába. További részletekért fordulj a galéria adminisztrátorához.';
+$lang['Error_HIPE_BlacklistedIP'] = 'Hiba! Az IP címe le van tiltva, nem regisztrálhat a galériába. További részletekért forduljon a galéria adminisztrátorához.';
 // --------- End: New or revised $lang ---- from version 2.1.1
 ?>

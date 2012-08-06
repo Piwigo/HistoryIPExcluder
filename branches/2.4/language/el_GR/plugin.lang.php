1ee935a5ec19048cb21753471e453fdca57847da
Eric <Eric@piwigo.org>
2012-08-06 19:12:11 +0000

    Merge r17413 from trunk to branch 2.4
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@17440 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ branches/2.4/language/el_GR/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Error_HIPE_BlacklistedIP'] = 'Σφάλμα! Η IP σας έχει απαγορευτεί. Δεν μπορείτε να εγγραφείτε σε αυτή την γκαλερί. Επικοινωνήστε με τον διαχειριστή για περισσότερες λεπτομέρειες.';
+$lang['HIPE_CleanHist'] = 'Καθαρισμός Ιστορικού';
+$lang['HIPE_IPBlacklist_title'] = 'Μαύρη λίστα εγγραφής';
+$lang['HIPE_IPBlacklisted'] = 'Αποτροπή εγγραφής στη γκαλερί των αποκλεισμένων IPs (μαύρη λίστα)';
+$lang['HIPE_IPByMember_description'] = 'Εμφάνιση των IPs που χρησιμοποιούνται από τα μέλη, ταξινόμηση με βάση το IP';
+$lang['HIPE_IPByMember'] = 'IPs κατά μέλος';
+$lang['HIPE_IPForMember_description'] = 'Αναζήτηση και εμφάνιση των IPs που σχετίζονται με ένα εγγεγραμμένο χρήστη (ταξινομημένο κατά IP)';
+$lang['HIPE_IPForMember'] = 'IPs ενός μέλους';
+$lang['HIPE_IPnoGuest_description'] = ' ';
+$lang['HIPE_IPnoGuest'] = ' ';
+$lang['HIPE_MemberForIp_description'] = 'Αναζήτηση και εμφάνιση των χρηστών που συνδέονται με μία IP (ταξινομημένο κατά όνομα)';
+$lang['HIPE_MemberForIp'] = 'Μέλη για μία IP';
+$lang['HIPE_OnlyGuest_description'] = 'Δείτε μόνο τα IPs ρησιμοποιείται από καλεσμένους και τον αριθμό του πόσες φορές που έχει βρεθεί στον πίνακα του ιστορικού, ταξινομημένα κατά τον αριθμό των φορών στο ιστορικό';
+$lang['HIPE_OnlyGuest'] = 'Τα IPs μόνο επισκεπτών';
+$lang['HIPE_admin_description1'] = 'Πληκτρολογήστε το πλήρες IP ή εύρος IP για να αποκλείσετε (ένα ανά γραμμή) στο παρακάτω πλαίσιο. Για να καθορίσετε μια λίστα IP, χρησιμοποιήστε το χαρακτήρα μπαλαντέρ "%".<br> Παράδειγμα:. 74.6.1.2 ή 74.6.%';
+$lang['HIPE_admin_section1'] = 'Εξαίρεση IP';
+$lang['HIPE_admin_section2'] = 'Ερωτήματα σχετικά με τον πίνακα Ιστορικού';
+$lang['HIPE_admin_section3'] = 'Αποτέλεσμα του αιτήματος του ιστορικού ';
+$lang['HIPE_description'] = 'Αυτό το πρόσθετο επιτρέπει να εξαιρεθούν από το ιστορικό και τα στατιστικά στοιχεία των IP ή σειρών IP.<br>Η ενεργοποίηση του μπλοκάρει τις εγγραφές στον πίνακα των IP στο Ιστορικό που προσδιορίζονται στον παρακάτω πίνακα.
+';
+$lang['HIPE_hist_cleaned'] = 'Έγινε καθαρισμός του πίνακα του ιστορικού';
+$lang['HIPE_resquet_ok'] = 'Αίτηση ΟΚ.';
+$lang['HIPE_save_config'] = 'Οι ρυθμίσεις αποθηκεύτηκαν ';
+$lang['HIPE_version'] = '- Έκδοση: ';
+$lang['IP_geolocalisation'] = 'Γεωγραφικός Προσδιορισμός';
+?>

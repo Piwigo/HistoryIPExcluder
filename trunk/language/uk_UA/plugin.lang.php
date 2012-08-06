cb0cbe2f80f42f74ee522883588c35eaed005296
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-06 05:49:15 +0000

    [history_ip_excluder] Add uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@17413 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ trunk/language/uk_UA/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_admin_description1'] = 'Введіть повний IP-адрес або діапазон IP-адрес для виключення (по одному на рядок) в поле нижче. Щоб задати IP-діапазон, використовуйте символ підстановки "%".<br> Приклад: 74.6.1.2 або 74.6.%';
+$lang['HIPE_MemberForIp'] = 'Користувачі з одним IP';
+$lang['HIPE_admin_section1'] = 'Виняток IP-адреси';
+$lang['HIPE_IPnoGuest'] = 'Переклад відсутній';
+$lang['HIPE_IPnoGuest_description'] = 'Переклад відсутній';
+$lang['HIPE_OnlyGuest_description'] = 'Показати IP-адреси, які використовуються тільки гостями та скільки разів вони знаходиться в таблиці історії, відсортовані за кількістю раз';
+$lang['HIPE_IPBlacklisted'] = 'Запобігання реєстрації в галереї виключених IP-адреси (чорний список)';
+$lang['HIPE_IPForMember_description'] = 'Пошук і відображення IP-адрес пов\'язаних із зареєстрованим користувачем (відсортовані по IP)';
+$lang['HIPE_MemberForIp_description'] = 'Пошук і відображення користувачів, що додається до однієї IP (відсортовані за іменами)';
+$lang['HIPE_description'] = 'Цей плагін дозволяє виключити з історії та статистики IP-адресу або діапазон IP-адрес. <br> Його активація блокує записи в таблиці IP-адрес *_history, зазначених у таблиці нижче.';
+$lang['Error_HIPE_BlacklistedIP'] = 'Помилка! Ваш IP-адрес був заборонений. Ви не можете підписатися на цю галерею. Зверніться до адміністратора для уточнення деталей.';
+$lang['HIPE_IPByMember_description'] = 'Показати IP - адрес, що використовуються користувачами, відсортовані за IP';
+$lang['HIPE_OnlyGuest'] = 'Тільки IP-адреси гостей';
+$lang['HIPE_admin_section2'] = 'Питання з таблиці історії';
+$lang['HIPE_resquet_ok'] = 'Запит ОК.';
+$lang['HIPE_save_config'] = 'Налаштування збережені.';
+$lang['HIPE_version'] = '- Версія:';
+$lang['IP_geolocalisation'] = 'Геолокалізація';
+$lang['HIPE_CleanHist'] = 'Очищення історії';
+$lang['HIPE_IPBlacklist_title'] = 'Реєстрація чорного списку';
+$lang['HIPE_IPByMember'] = 'IP-адреси користувача';
+$lang['HIPE_IPForMember'] = 'IP-адреси для користувача';
+$lang['HIPE_admin_section3'] = 'Результат історичного запиту';
+$lang['HIPE_hist_cleaned'] = 'Очищення таблиці історії зроблено';
+?>

807c46096012531fb82a9980c5dde1e0f7d4dc3d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-20 16:55:29 +0000

    [history_ip_excluder] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@17600 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,31 +1,33 @@
 <?php
-$lang['HIPE_description'] = 'Este plugin permite excluir de la reseña histórica y las estadísticas del IP o las playas d \'IP. <br> Su activación bloquea el registro en la mesa * _history IP especificados en el tablero más abajo.';
+$lang['HIPE_description'] = 'Este plugin permite excluir del historial y las estadísticas de rangos de IP o IP. <br> sus bloques de activación registro de la tabla de IP *_Historia especificada en la tabla de abajo.';
 $lang['HIPE_admin_section1'] = 'IP a excluir';
-$lang['HIPE_admin_description1'] = 'Coja el IP completo o las playas de IP a excluir (Uno por línea) En el marco(ejecutivo) más abajo. Para indicar una playa de IP, utilice el carácter mono "%".<br>Por Ejemplo : 74.6.2.1 o 74.6.%<br><br>';
+$lang['HIPE_admin_description1'] = 'Introduzca la dirección IP completa o rangos de IP que desea excluir (uno por línea) en el cuadro de abajo. Para especificar un rango de direcciones IP, utilice el carácter comodín "%" <br> Ejemplo:.. 74.6.1.2 o 74,6%';
 $lang['HIPE_save_config']='Configuración registrada.';
-$lang['HIPE_CleanHist']='Limpiar la reseña histórica';
+$lang['HIPE_CleanHist'] = 'Limpiar historial';
 
-$lang['HIPE_admin_section2'] = 'Demandas sobre la reseña histórica';
-$lang['HIPE_admin_section3'] = 'El resultado de la demanda sobre la reseña histórica';
+$lang['HIPE_admin_section2'] = 'Consultas sobre tabla de historial';
+$lang['HIPE_admin_section3'] = 'Resultado sobre consulta de tabla de historial';
 $lang['HIPE_IPByMember'] = 'IP de usuarios';
-$lang['HIPE_IPByMember_description'] = 'Búsqueda y fija el IP de usuarios inscritos, (selección por IP)';
+$lang['HIPE_IPByMember_description'] = 'Búsqueda y muestra el IP de usuarios inscritos, (selección por IP)';
 $lang['HIPE_OnlyGuest'] = 'IP de invitados solamente';
-$lang['HIPE_OnlyGuest_description'] = 'Búsqueda y anuncio únicamente el IP utilizados por invitados, y el número de visitas asociados (selección por número de visitas por IP)';
+$lang['HIPE_OnlyGuest_description'] = 'Búsqua y muestra únicamente el IP utilizado por invitados, y el número de visitas asociadas (selección por número de visitas por IP)';
 $lang['HIPE_IPnoGuest'] = '';
 $lang['HIPE_IPnoGuest_description'] = '';
 
-$lang['HIPE_IPForMember'] = 'IP de un usuario';
-$lang['HIPE_IPForMember_description'] = 'Búsqueda y fija el IP asociados con un usuario inscrito (selección por IP)';
-$lang['HIPE_MemberForIp'] = 'Usuarios de un IP';
-$lang['HIPE_MemberForIp_description'] = 'Búsqueda y fija a los usuarios pegados a un IP (selección por usuario)';
+$lang['HIPE_IPForMember'] = 'IP de usuario';
+$lang['HIPE_IPForMember_description'] = 'Búsqueda y muestra IP asociados con un usuario inscrito (selección por IP)';
+$lang['HIPE_MemberForIp'] = 'Usuarios de IP';
+$lang['HIPE_MemberForIp_description'] = 'Búsquea y muestra a los usuarios relacionados con un IP (selección por usuario)';
 
 $lang['HIPE_resquet_ok'] = 'Demanda ejecutada.';
-$lang['HIPE_hist_cleaned'] = 'Limpieza de la reseña histórica efectuada.';
+$lang['HIPE_hist_cleaned'] = 'Limpieza de la tabla del historial, hecho.';
 
 $lang['IP_geolocalisation'] = 'Geolocalización';
-$lang['submit'] = 'Someter';
 
 // --------- Starting below: New or revised $lang ---- from version 2.1.0
 $lang['HIPE_version'] = ' - Versión: ';
 // --------- End: New or revised $lang ---- from version 2.1.0
+$lang['Error_HIPE_BlacklistedIP'] = 'Error: Su IP ha sido prohibido. Usted no puede suscribirse a esta galería. Póngase en contacto con el administrador para obtener más detalles.';
+$lang['HIPE_IPBlacklist_title'] = 'Registro de lista negra';
+$lang['HIPE_IPBlacklisted'] = 'Impedir el registro a la galería de IPs excluidos (lista negra)';
 ?>

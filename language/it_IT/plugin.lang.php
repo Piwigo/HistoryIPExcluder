49ba4a624d11ffbb88f992295c68f76a328fd678
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-09 17:59:26 +0000

    [history_ip_excluder] Update it_IT, thanks to : virgigiole
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@17836 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,26 +1,26 @@
 <?php
-$lang['HIPE_description'] = 'Questo plugin esclude gli indirizzi IP o intervalli d\indieizzi dalla cronologgia e dalle statistiche.<br>Attivandolo, gli indirizzi IP specifici della tabella sottostante non saranno più registrati nella tabella *.history de PWG.';
+$lang['HIPE_description'] = 'Questo plugin esclude gli indirizzi IP o intervalli di indirizzi dalla cronologia e dalle statistiche.<br>Attivandolo, gli indirizzi IP specifici della tabella sottostante non saranno piÃ¹ registrati nella tabella *.history di PWG.';
 $lang['HIPE_admin_section1'] = 'Esclusione d\'IP';
 $lang['HIPE_admin_description1'] = 'Riempite le tabella sottostante con gli indirizzi IP o intervalli da escludere (uno per riga). Potete usare degli IP completi o una serie usando il carattere jolly "%".<br>Per esempio : 74.6.2.1 o 74.6.%<br><br>';
 $lang['HIPE_save_config']='Configurazione registrata.';
-$lang['HIPE_CleanHist']='Ripulire la cronologgia';
+$lang['HIPE_CleanHist'] = 'Ripulire la cronologia';
 
-$lang['HIPE_admin_section2'] = 'Selezioni sulla cronologgia';
-$lang['HIPE_admin_section3'] = 'Risultato della selezione';
+$lang['HIPE_admin_section2'] = 'Interrogazioni sulla cronologia';
+$lang['HIPE_admin_section3'] = 'Risultato dell\'interrogazione';
 $lang['HIPE_IPByMember'] = 'IP per utente';
-$lang['HIPE_IPByMember_description'] = 'Visualizza gli indirizzi IP legati a degli utenti, per indirizzo';
+$lang['HIPE_IPByMember_description'] = 'Visualizza gli indirizzi IP legati a degli utenti, ordinati per indirizzo';
 $lang['HIPE_OnlyGuest'] = 'IP solo degli ospiti';
 $lang['HIPE_OnlyGuest_description'] = 'Visualizza gli indirizzi IP usati unicamente dagli ospiti e il numero di volte che appaiono nella base, per numero di volte per IP';
 $lang['HIPE_IPnoGuest'] = '';
 $lang['HIPE_IPnoGuest_description'] = '';
 
 $lang['HIPE_IPForMember'] = 'IP di un\'utente';
-$lang['HIPE_IPForMember_description'] = 'Visualizza gli indirizzi IP legati agli utenti, per IP';
+$lang['HIPE_IPForMember_description'] = 'Cerca e visualizza gli indirizzi IP legati agli utenti (ordinati per IP)';
 $lang['HIPE_MemberForIp'] = 'Utenti di un\'indirizzo IP';
-$lang['HIPE_MemberForIp_description'] = 'Visualizza gli utenti legati ad un\'indirizzo IP, per utente';
+$lang['HIPE_MemberForIp_description'] = 'Visualizza gli utenti legati ad un\'indirizzo IP (ordinati per utente)';
 
-$lang['HIPE_resquet_ok'] = 'Esecuzzione della selezzione riuscita.';
-$lang['HIPE_hist_cleaned'] = 'Operazzione di "pulizzia" della cronologgia riuscito.';
+$lang['HIPE_resquet_ok'] = 'Selezione riuscita.';
+$lang['HIPE_hist_cleaned'] = 'Operazzione di "pulizia" della cronologia riuscito.';
 
 $lang['IP_geolocalisation'] = 'Geolocalizzazione';
 

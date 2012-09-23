92220e28fa5023e9bced0a218d31afc81c86f8f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-23 19:01:51 +0000

    [history_ip_excluder] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@18188 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_IPnoGuest'] = '?';
+$lang['HIPE_IPnoGuest_description'] = '?';
+$lang['HIPE_admin_description1'] = 'Do poľa nižšie zadajte kompletné IP adresy, alebo rozsah IP adries pre zaradenie do blacklistu (jednu adresu na riadok). Pre upresnenie rozsahu IP adries použite rozšírený znak "%".<br>Napríklad: 74.6.1.2 alebo 74.6.%';
+$lang['HIPE_description'] = 'Tento doplnok umožňuje vynechať IP adresy, alebo rozsahy IP adries z histórie a štatistík. <br>Jeho aktivácia blokuje záznamy v tabuľke histórie IP adries uvedené v tabuľke nižšie.';
+$lang['HIPE_admin_section2'] = 'Otázky na tabuľku histórie';
+$lang['HIPE_OnlyGuest_description'] = 'Ukáže len IP použité hosťami a počet, koľkokrát bolo nájdené v tabuľke, triedené podľa počtu';
+$lang['HIPE_IPForMember_description'] = 'Hľadať a ukázať IP spojenú s registrovaným používateľom (zoradené podľa IP)';
+$lang['Error_HIPE_BlacklistedIP'] = 'Chyba! Vaša IP adresa je blokovaná a preto sa nemôžete zaregistrovať do tejto galérie. Ak Vás zaujímajú podrobnosti, kontaktujte administrátora galérie.';
+$lang['HIPE_MemberForIp_description'] = 'Vyber a hľadaj používateľov pridružených k jednej IP (zoradenie podľa mena)';
+$lang['HIPE_admin_section3'] = 'Výsledok historickej žiadosti';
+$lang['HIPE_hist_cleaned'] = 'Vyčistenie tabuľky histórie uskutočnené';
+$lang['IP_geolocalisation'] = 'Geolokalizácia';
+$lang['HIPE_version'] = '-verzia:';
+$lang['HIPE_save_config'] = 'Konfigurácia uložená';
+$lang['HIPE_resquet_ok'] = 'Žiadosť OK';
+$lang['HIPE_admin_section1'] = 'Vylúčenie IP';
+$lang['HIPE_OnlyGuest'] = 'Len IP hostí';
+$lang['HIPE_MemberForIp'] = 'Používatelia jednej IP';
+$lang['HIPE_IPByMember_description'] = 'Ukázať IP používateľov zoradené podľa IP';
+$lang['HIPE_IPForMember'] = 'IP pre používateľa';
+$lang['HIPE_IPByMember'] = 'IP podľa používateľa';
+$lang['HIPE_IPBlacklisted'] = 'Zabrániť registrácii používateľov s nasledujúcimi IP adresami (blacklist)';
+$lang['HIPE_IPBlacklist_title'] = 'Registrácia blacklistu';
+$lang['HIPE_CleanHist'] = 'Vyčistiť históriu';
+?>

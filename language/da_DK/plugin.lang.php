f6693f08b4016ebf8fef4efb2dfc099e6fd167ce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-02 18:50:36 +0000

    [history_ip_excluder] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@18442 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_IPnoGuest'] = ' ';
+$lang['HIPE_IPnoGuest_description'] = ' ';
+$lang['HIPE_resquet_ok'] = 'Forespørgsel er ok.';
+$lang['HIPE_save_config'] = 'Opsætning er gemt.';
+$lang['HIPE_version'] = ' - Version:';
+$lang['IP_geolocalisation'] = 'Geoplacering';
+$lang['Error_HIPE_BlacklistedIP'] = 'Fejl! Din IP-adresse er blevet bandlyst. Du kan ikke tilmelde dig dette galleri. Kontakt administratoren for flere oplysninger.';
+$lang['HIPE_CleanHist'] = 'Tøm historik';
+$lang['HIPE_IPBlacklist_title'] = 'Registreringssortliste';
+$lang['HIPE_IPBlacklisted'] = 'Forhindrer at fravalgte (sortlistede) IP-adresser kan registrere sig i galleriet';
+$lang['HIPE_IPByMember'] = 'IP-adresser efter medlem';
+$lang['HIPE_IPByMember_description'] = 'Vis IP-adresser benyttet af medlemmer, sorteret efter IP-adresse';
+$lang['HIPE_IPForMember'] = 'Medlems IP-adresse';
+$lang['HIPE_IPForMember_description'] = 'Fremsøg og vis IP-adresser knyttet til en registreret bruger (sorteret efter IP-adresse)';
+$lang['HIPE_MemberForIp'] = 'Medlemmer vedrørende en IP-adresse';
+$lang['HIPE_MemberForIp_description'] = 'Fremsøg og vis brugere knyttet til en IP-adresse (sorteret efter navn)';
+$lang['HIPE_OnlyGuest'] = 'Kun gæsters IP-adresser';
+$lang['HIPE_OnlyGuest_description'] = 'Hvis kun IP-adresser, som benyttes af gæster og antal gange de er fundet i historiktabellen, sorteret efter antal gange';
+$lang['HIPE_admin_description1'] = 'Skriv den komplette IP-adresse eller interval af IP-adresser (en pr. linje) i boksen herunder. For at angive et interval af IP-adresser benyttes wildcard-tegnet "%".<br>Eksempel: 74.6.1.2 eller 74.6.%';
+$lang['HIPE_admin_section1'] = 'IP-fravalg';
+$lang['HIPE_admin_section2'] = 'Forespørgsler i historiktabellen';
+$lang['HIPE_admin_section3'] = 'Resultat af historikforespørgslen';
+$lang['HIPE_description'] = 'Plugin\'en gør det muligt at fravælge IP-adresser og intervaller af IP-adresser fra historik og statistik.<br>Dens aktiveringsblok gemmer i IP-adressetabellen *_history angivet herunder.';
+$lang['HIPE_hist_cleaned'] = 'Tømning af historiktabellen er udført.';
+?>

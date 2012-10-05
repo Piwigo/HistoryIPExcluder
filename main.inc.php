c4baacfe6049092288530025b3e5200f5c673321
Eric <Eric@piwigo.org>
2012-10-05 20:26:56 +0000

    Next version is 2.4.4 : Update uk_UA, thanks to : animan Update es_ES, thanks to : jpr928 Update it_IT, thanks to : virgigiole Update sk_SK, thanks to : dodo Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@18486 68402e56-0260-453c-a942-63ccdbb3a9ee

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: History IP Excluder
-Version: 2.4.2
+Version: auto
 Description: Permet l'exclusion d'une IP ou d'une plage d'IP de l'historique et de les blacklister à l'inscription / Excludes one IP or a range of IP from the history and to blacklist them on registration
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=147
 Author: Nicco, Eric
@@ -43,7 +43,19 @@ Author URI: http://gallery-nicco.no-ip.org - http://www.infernoweb.net
                   - Add  ru_RU translation (thanks to nadusha)
 
 2.4.2             - Update ru_RU translation (thanks to nadusha)
-                  
+
+2.4.3             - Update hu_HU translation (thanks to samli)
+                  - Update pl_PL translation (thanks to kuba)
+                  - Update cs_CZ translation (thanks to sichr)
+                  - Update el_GR translation (thanks to bas_alba)
+                  - Update lv_LV translation (thanks to agrisans)
+                  - Add uk_UA translation (thanks to animan)
+
+2.4.4             - Update uk_UA, thanks to : animan
+                  - Update es_ES, thanks to : jpr928
+                  - Update it_IT, thanks to : virgigiole
+                  - Update sk_SK, thanks to : dodo
+                  - Add da_DK, thanks to : Kaare
 --------------------------------------------------------------------------------
 */
 

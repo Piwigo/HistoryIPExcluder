a51da6b9bdf0d2c0ae0b49cc9074e7b4d5b3be08
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-10 07:04:43 +0000

    [history_ip_excluder] Add tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@18979 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_description'] = 'Bu eklenti,bir IP veya IP aralığına ait istatistikleri ve geçmişi dışlar. <br> IP *_history etkinleştirme blok kayıtları aşağıdaki tabloda belirtilmiştir.';
+$lang['HIPE_admin_description1'] = 'Aşağıdaki alana her satıra bir adet olacak şekilde dışlanacak tam IP veya IP aralığını girin. IP aralığı seçerken "%" joker karakterini kullanın.<br>Örnek : 74.6.1.2 yada 74.6.%';
+$lang['HIPE_OnlyGuest_description'] = 'Sadece misafiler tarafından kullanılmış IP numaralarını ve kaç kez kullandıklarını kullanım sayısına göre sıralı şekilde göster';
+$lang['HIPE_IPForMember_description'] = 'Kayıtlı kullanıcılara atanmış IP numaralarını (IP\'ye göre sıralı) ara ve görüntüle';
+$lang['HIPE_MemberForIp_description'] = 'Bir IP numarası atanan kullanıcıları (isme göre sıralı) ara ve görüntüle';
+$lang['HIPE_OnlyGuest'] = 'Sadece Misafir IP numaraları';
+$lang['IP_geolocalisation'] = 'Dünyadaki konum';
+$lang['HIPE_admin_section3'] = 'Geçmiş isteği sonucu';
+$lang['HIPE_admin_section2'] = 'Geçmiş tablosunda sorgulama';
+$lang['HIPE_admin_section1'] = 'IP Dışlama';
+$lang['HIPE_IPnoGuest_description'] = ' ';
+$lang['HIPE_IPnoGuest'] = ' ';
+$lang['HIPE_hist_cleaned'] = 'Geçmiş tablosu silindi.';
+$lang['HIPE_resquet_ok'] = 'İstek TAMAM.';
+$lang['HIPE_save_config'] = 'Yapılandırma kaydedildi.';
+$lang['HIPE_version'] = ' - Sürüm:';
+$lang['HIPE_MemberForIp'] = 'Bir IP \'deki üyeler';
+$lang['HIPE_IPByMember_description'] = 'Üyelerin kullandığı IP numaralarını göster, IP \'ye göre sıralı';
+$lang['HIPE_IPForMember'] = 'Bir üye için IP numaraları';
+$lang['HIPE_IPByMember'] = 'Üye IP numaraları';
+$lang['HIPE_IPBlacklisted'] = 'Çıkarılan IP\'lerin (kara liste) galeriye üye olmalarını engelle';
+$lang['HIPE_IPBlacklist_title'] = 'Kayıt kara listesi';
+$lang['HIPE_CleanHist'] = 'Geçmişi Temizle';
+$lang['Error_HIPE_BlacklistedIP'] = 'Hata! IP numaranız yasaklandı. Bu galeriye üye olamazsınız. Daha fazla detay için yönetici ile bağlantı kurun.';
+?>

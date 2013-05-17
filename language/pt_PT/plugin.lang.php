e1671af9765aeec1fad71d3d5251fe3c49e5d81c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-17 22:45:44 +0000

    [history_ip_excluder] Add pt_PT, thanks to : ddtddt & ANO
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@22726 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_IPnoGuest'] = ' ';
+$lang['HIPE_IPnoGuest_description'] = ' ';
+$lang['HIPE_save_config'] = 'Configuração salva';
+$lang['HIPE_description'] = 'Esta extensão permite excluir do histórico e das estatísticas de IP ou  faixa de IP\'s. <br> A ativação bloqueia o registo na tabela de IP*_history especificado na tabela abaixo.';
+$lang['HIPE_hist_cleaned'] = 'Limpeza da tabela histórico executada';
+$lang['HIPE_resquet_ok'] = 'Pedido Ok.';
+$lang['HIPE_version'] = '- Versão:';
+$lang['IP_geolocalisation'] = 'Geolocalização';
+$lang['HIPE_IPBlacklisted'] = 'Inibir registo na galeria aos IP\'s excluídos (Lista negra)';
+$lang['HIPE_MemberForIp'] = 'Meembros para um IP';
+$lang['HIPE_MemberForIp_description'] = 'Procurar e mostrar utilizadores anexados a um IP(Organizar por nome)';
+$lang['HIPE_OnlyGuest'] = 'Apenas IP\'s visitantes';
+$lang['HIPE_OnlyGuest_description'] = 'Mostrar apenas os IPs utilizados por visitantes e o número de vezes que os mesmos existem na tabela do historial, ordenados pelo número de vezes usados. ';
+$lang['HIPE_admin_description1'] = 'Introduzir, na caixa abaixo, o IP completo ou amplitude de IP\'s a excluir (um por linha). Para especificar uma faixa de IP\'s, use o caractére percentagem "%" <br> Exemplo:.. 74.6.1.2 ou 74,6%';
+$lang['HIPE_admin_section1'] = 'Exclusão de IP';
+$lang['HIPE_admin_section2'] = 'Consultas sobre a tabela histórico';
+$lang['HIPE_admin_section3'] = 'Resultado do histórico solicitado';
+$lang['Error_HIPE_BlacklistedIP'] = 'Erro! Seu IP foi banido. Você não pode inscrever-se nesta galeria. Contacte o administrador para obter mais detalhes.';
+$lang['HIPE_CleanHist'] = 'Apagar historial';
+$lang['HIPE_IPBlacklist_title'] = 'Lista negra de registos';
+$lang['HIPE_IPByMember'] = 'IP\'s por membro';
+$lang['HIPE_IPByMember_description'] = 'Mostrar IP\'s usados pelos membros, ordenar por IP';
+$lang['HIPE_IPForMember'] = 'IP\'s para um membro';
+$lang['HIPE_IPForMember_description'] = 'Procurar e mostrar os IP\'s associados com um utilizador registado(ordenar por IP)';
+?>

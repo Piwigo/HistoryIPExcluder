ea6adc45e28e107bde9f0c6728132603a23555e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-07 14:55:03 +0000

    [history_ip_excluder] Add nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@23087 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Error_HIPE_BlacklistedIP'] = 'Fout! Uw IP is verboden (staat op de blacklist). U kunt zich niet registreren op deze galerie. Neem contact op met de beheerder voor nadere informatie';
+$lang['HIPE_IPBlacklist_title'] = 'Registratie blacklist';
+$lang['HIPE_IPBlacklisted'] = 'Voorkom registratie bij uw galerie door geblokkeerde IP\'s (blacklist)';
+$lang['HIPE_IPByMember'] = 'IP-adressen gesorteerd op geregistreerde';
+$lang['HIPE_IPForMember'] = 'IP\'s voor een geregistreerde';
+$lang['HIPE_MemberForIp'] = 'Geregistreerden ';
+$lang['HIPE_MemberForIp_description'] = 'Zoek en geef de gebruikers weer die gekoppeld zijn aan een IP-adres (gesorteerd op naam)';
+$lang['HIPE_OnlyGuest'] = 'Alleen Gast-IP\'s';
+$lang['HIPE_OnlyGuest_description'] = 'Geef de door Gasten gebruikte IP-adressen weer en het aantal keren dat het IP-adres in het geschiedenis-bestand is gevonden, gesorteerd op het aantal keren';
+$lang['HIPE_admin_description1'] = 'Geef hieronder het volledige IP-adres of de IP-reeksen (één per regel) die uitgesloten moeten worden. Om een IP-bereik op te geven, kunt u het jokerteken "%" gebruiken.<br> Bijvoorbeeld: 74.6.1.2 of 74.6.%';
+$lang['HIPE_admin_section1'] = 'IP uitsluiting';
+$lang['HIPE_admin_section2'] = 'Vragen aan het geschiedenisbestand';
+$lang['HIPE_admin_section3'] = 'Resultaat van het verzoek aan het geschiedenisbestand';
+$lang['HIPE_description'] = 'Deze plugin maakt het mogelijk om IP-adressen of IP-reeksen uit te sluiten van de geschiedenis en de statistieken. <br> Het activeren van deze plugin voorkomt dat de gegevens van deze IP-adressen in het geschiedenisbestand worden opgenomen.';
+$lang['HIPE_resquet_ok'] = 'Vraag OK.';
+$lang['HIPE_IPByMember_description'] = 'Laat de IP\'s zien, die worden gebruikt door leden, gesorteerd op IP';
+$lang['HIPE_IPForMember_description'] = 'Zoek en geef de IP\'s, gekoppeld aan een geregistreerde gebruiker, weer (gesorteerd op IP)';
+$lang['HIPE_hist_cleaned'] = 'Opschonen van het geschiedenisbestand is klaar.';
+$lang['HIPE_save_config'] = 'Configuratie opgeslagen.';
+$lang['HIPE_version'] = '-Versie:';
+$lang['IP_geolocalisation'] = 'Geolocalisatie';
+$lang['HIPE_CleanHist'] = 'Schoon geschiedenis';
+?>

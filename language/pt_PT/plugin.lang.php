c5e88210315336005ac9fce4f7048bace23e0e54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-30 10:37:04 +0000

    [history_ip_excluder] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@23698 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_PT/plugin.lang.php
+++ language/pt_PT/plugin.lang.php
@@ -27,7 +27,7 @@ $lang['HIPE_resquet_ok'] = 'Pedido Ok.';
 $lang['HIPE_version'] = '- Versão:';
 $lang['IP_geolocalisation'] = 'Geolocalização';
 $lang['HIPE_IPBlacklisted'] = 'Inibir registo na galeria aos IP\'s excluídos (Lista negra)';
-$lang['HIPE_MemberForIp'] = 'Meembros para um IP';
+$lang['HIPE_MemberForIp'] = 'Membros para um IP';
 $lang['HIPE_MemberForIp_description'] = 'Procurar e mostrar utilizadores anexados a um IP(Organizar por nome)';
 $lang['HIPE_OnlyGuest'] = 'Apenas IP\'s visitantes';
 $lang['HIPE_OnlyGuest_description'] = 'Mostrar apenas os IPs utilizados por visitantes e o número de vezes que os mesmos existem na tabela do historial, ordenados pelo número de vezes usados. ';
@@ -42,4 +42,5 @@ $lang['HIPE_IPByMember'] = 'IP\'s por membro';
 $lang['HIPE_IPByMember_description'] = 'Mostrar IP\'s usados pelos membros, ordenar por IP';
 $lang['HIPE_IPForMember'] = 'IP\'s para um membro';
 $lang['HIPE_IPForMember_description'] = 'Procurar e mostrar os IP\'s associados com um utilizador registado(ordenar por IP)';
+$lang['submit'] = 'Submater';
 ?>

252a67133f2897d39b37b8766ec13f5faf0cb7ec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-15 05:35:33 +0000

    [history_ip_excluder] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@28704 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_admin_description1'] = '在下面的框中输入完整的IP或一个IP范围，以将其列为拒绝的IP（每行1个）。指定一个IP范围，请使用通配符 "%" 。<br>如： 74.6.1.2 或 74.6.%';
+$lang['HIPE_OnlyGuest_description'] = '显示（仅限）访客的IP，及其在历史表单中出现的次数';
+$lang['HIPE_IPBlacklisted'] = '禁止所拒绝的IP注册相册（黑名单）';
+$lang['HIPE_IPByMember'] = '用户使用的IP';
+$lang['HIPE_IPByMember_description'] = '显示用户使用的IP（按IP顺序排列）';
+$lang['HIPE_admin_section1'] = '拒绝IP';
+$lang['HIPE_admin_section2'] = '查询历史表单';
+$lang['HIPE_admin_section3'] = '历史请求的结果';
+$lang['HIPE_description'] = '本插件可从IP或IP范围的历史统计中拒绝IP。 <br>启用它将封禁以下表单中所列的IP记录。';
+$lang['HIPE_IPForMember'] = '用户的IP';
+$lang['HIPE_IPForMember_description'] = '搜索并显示与一个注册用户相关联的IP（按IP顺序排列）';
+$lang['HIPE_MemberForIp'] = 'IP的用户';
+$lang['HIPE_MemberForIp_description'] = '搜索并显示与一个IP相关联的用户（按用户名顺序排列）';
+$lang['Error_HIPE_BlacklistedIP'] = '错误！您的IP已被封禁。您无法订阅本相册。进一步细节请联系管理员。';
+$lang['HIPE_OnlyGuest'] = '仅访客IP';
+$lang['HIPE_hist_cleaned'] = '清除历史表单完成。';
+$lang['HIPE_CleanHist'] = '清除历史';
+$lang['HIPE_IPBlacklist_title'] = '注册黑名单';
+$lang['HIPE_resquet_ok'] = '通过请求。';
+$lang['HIPE_save_config'] = '设置已保存。';
+$lang['HIPE_version'] = '- 版本： ';
+$lang['IP_geolocalisation'] = '地理定位';
+$lang['submit'] = '提交';

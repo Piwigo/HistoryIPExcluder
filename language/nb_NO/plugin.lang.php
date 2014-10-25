a8c0d0b505366b7761fca8ab6ab8ba130f0dc5f4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-25 19:30:36 +0000

    [history_ip_excluder] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/HistoryIPExcluder@30308 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Error_HIPE_BlacklistedIP'] = 'Feil! Din IP har blitt utestengt. Du kan ikke abonnere på dette galleriet. Kontakt administrator for ytterligere detaljer.';
+$lang['HIPE_CleanHist'] = 'Tøm historikk';
+$lang['HIPE_IPBlacklist_title'] = 'Registrering svarteliste';
+$lang['HIPE_IPBlacklisted'] = 'Forhindre påmelding til galleriet av IP-adresser som er ekskluderte (svarteliste)';
+$lang['HIPE_IPByMember'] = 'IP-adresser etter medlem';
+$lang['HIPE_IPByMember_description'] = 'Viser IP-adresser som brukes av medlemmer, sortert etter IP';
+$lang['HIPE_IPForMember'] = 'IP-adresse for et medlem';
+$lang['HIPE_IPForMember_description'] = 'Søk og vis IP-adresser asosiert med en registrert bruker (sortert etter IP)';
+$lang['HIPE_MemberForIp'] = 'Medlemmene for en IP';
+$lang['HIPE_MemberForIp_description'] = 'Søk og vis brukere tilknyttet til en IP (sortert etter navn)';
+$lang['HIPE_OnlyGuest'] = 'Kun Gjesters IP-adresser';

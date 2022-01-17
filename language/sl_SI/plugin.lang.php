197589fb76fa37097b0c6b369eef4ce55c7bcd8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 13:04:36 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_admin_section1'] = 'Izključitev IP';
+$lang['HIPE_admin_section2'] = 'Poizvedbe v tabeli zgodovine';
+$lang['HIPE_admin_section3'] = 'Rezultat zgodovinske zahteve';
+$lang['HIPE_description'] = 'Ta vtičnik omogoča izključitev obsegov IP ali IP iz zgodovine in statistike. <br>Njegova aktivacija blokira zapis v tabeli IP *_history, določeni v spodnji tabeli.';
+$lang['HIPE_hist_cleaned'] = 'Opravljeno je čiščenje tabele zgodovine.';
+$lang['HIPE_resquet_ok'] = 'Zahtevek v redu.';
+$lang['HIPE_save_config'] = 'Konfiguracija je shranjena.';
+$lang['IP_geolocalisation'] = 'Geolokalizacija';
+$lang['submit'] = 'Pošlji';
+$lang['HIPE_version'] = ' - Verzija: ';
+$lang['HIPE_IPForMember_description'] = 'Iskanje in prikaz IP-jev, povezanih z registriranim uporabnikom (razvrščenih po IP)';
+$lang['HIPE_MemberForIp'] = 'Člani za en IP';
+$lang['HIPE_MemberForIp_description'] = 'Iskanje in prikazovanje uporabnikov, povezanih z enim IP-jem (razvrščeno po imenu)';
+$lang['HIPE_OnlyGuest'] = 'Samo IP-ji gostov';
+$lang['HIPE_OnlyGuest_description'] = 'Pokaži IP-je, ki jih uporabljajo samo gostje, in kolikokrat jih najdemo v tabeli zgodovine, razvrščenih po številu krat';
+$lang['HIPE_admin_description1'] = 'V spodnje polje vnesite celoten obseg IP ali IP, ki ga želite izključiti (ena na vrstico). Če želite določiti obseg IP, uporabite nadomestni znak "%".<br>Primer: 74.6.1.2 ali 74.6.%';
+$lang['HIPE_IPByMember_description'] = 'Prikaži IP-je, ki jih uporabljajo člani, razvrščene po IP-ju';
+$lang['HIPE_IPForMember'] = 'IP-ji za člana';
+$lang['HIPE_CleanHist'] = 'Počisti zgodovino';
+$lang['HIPE_IPBlacklist_title'] = 'Črni seznam registracije';
+$lang['HIPE_IPBlacklisted'] = ' Preprečite registracijo v galerijo izključenih IP-jev (črni seznam)';
+$lang['HIPE_IPByMember'] = 'IP-ji po članu';
+$lang['Error_HIPE_BlacklistedIP'] = 'Napaka! Vaš IP je bil prepovedan. Ne morete se naročiti na to galerijo. Za več podrobnosti se obrnite na skrbnika.';

c9b2d26c60c244f28d7b4069b2d74e1f8fc611eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-12 06:27:17 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_version'] = '- Útgáfa: ';
+$lang['IP_geolocalisation'] = 'Landfræðistaðsetning';
+$lang['submit'] = 'Senda';
+$lang['Error_HIPE_BlacklistedIP'] = 'Villa! IP-talan þín hefur verið bönnuð. Þú getur ekki gerst áskrifandi að þessu myndasafni. Hafðu samband við stjórnanda til að fá frekari upplýsingar.';
+$lang['HIPE_IPBlacklist_title'] = 'Svartlisti skráningar';
+$lang['HIPE_IPBlacklisted'] = 'Koma í veg fyrir skráningu í gallerí útilokaðra IP-talna (svartlisti)';
+$lang['HIPE_IPByMember'] = 'IP-tölur eftir meðlimi';
+$lang['HIPE_IPByMember_description'] = 'Sýndu IP-tölurnar sem meðlimir nota, raðað eftir IP-tölum';
+$lang['HIPE_IPForMember'] = 'IP-tölur fyrir meðlimi';
+$lang['HIPE_IPForMember_description'] = 'Leita og birta IP-tölur sem tengjast skráðum notanda (raðað eftir IP tölum)';
+$lang['HIPE_MemberForIp'] = 'Meðlimir fyrir eina IP';
+$lang['HIPE_MemberForIp_description'] = 'Leitaðu að og birtu notendur sem eru tengdir við eina IP tölu (raðað eftir nafni)';
+$lang['HIPE_OnlyGuest'] = 'Aðeins gesta IP tölur';
+$lang['HIPE_OnlyGuest_description'] = 'Sýna IP-tölurnar sem aðeins gestir nota og fjölda skipta sem þær finnast í sögutöflunni, raðað eftir fjölda skipta';
+$lang['HIPE_admin_description1'] = 'Sláðu inn heildar IP tölur eða IP tölusvið til að útiloka (eitt í hverja línu) í reitinn hér að neðan. Til að tilgreina IP tölusvið skaltu nota algildisstafinn "%".<br>Dæmi: 74.6.1.2 eða 74.6.%';
+$lang['HIPE_admin_section1'] = 'IP tölu útilokun';
+$lang['HIPE_admin_section2'] = 'Fyrirspurnir í sögutöflu';
+$lang['HIPE_admin_section3'] = 'Niðurstaða sögulegu beiðninnar';
+$lang['HIPE_description'] = 'Þessi viðbót gerir kleift að útiloka frá sögu og tölfræði IP talna eða IP talnasviða. <br> Virkjunarblokkir þess skrá í töfluna fyrir IP tölur *_sögu sem tilgreind er í töflunni hér að neðan.';
+$lang['HIPE_hist_cleaned'] = 'Hreinsun á sögutöflunni lokið.';
+$lang['HIPE_resquet_ok'] = 'Beiðni í lagi.';
+$lang['HIPE_save_config'] = 'Stilling vistuð.';
+$lang['HIPE_CleanHist'] = 'Hreinsa sögu';

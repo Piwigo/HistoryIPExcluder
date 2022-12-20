b744bbe1ec9d0203370712b0fedde66eb7c28e1b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 08:10:31 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_version'] = ' - Version:';
+$lang['IP_geolocalisation'] = 'Geolokalisering';
+$lang['submit'] = 'Skicka in';
+$lang['Error_HIPE_BlacklistedIP'] = 'Fel! Ditt IP har förbjudits. Du kan inte prenumerera på detta galleri. Kontakta administratören för mer information.';
+$lang['HIPE_CleanHist'] = 'Rensa historik';
+$lang['HIPE_IPBlacklist_title'] = 'Registrering blocklista';
+$lang['HIPE_IPBlacklisted'] = 'Förhindra registrering till galleriet för exkluderade IPs (blocklista)';
+$lang['HIPE_IPByMember'] = 'IPs efter medlem';
+$lang['HIPE_IPByMember_description'] = 'Visa IPs som används av medlemmar, sorterade efter IP';
+$lang['HIPE_IPForMember'] = 'IPs för en medlem';
+$lang['HIPE_IPForMember_description'] = 'Sök och visa IPs associerade med en registrerad användare (sorterade efter IP)';
+$lang['HIPE_MemberForIp'] = 'Medlemmar för ett IP';
+$lang['HIPE_MemberForIp_description'] = 'Sök och visa användare fäst till ett IP (sorterade efter namn)';
+$lang['HIPE_OnlyGuest'] = 'Endast gästers IP';
+$lang['HIPE_OnlyGuest_description'] = 'Visa IPs som bara används av gäster och antalet träffar i historiktabellen, sorterad efter antalet träffar';
+$lang['HIPE_admin_description1'] = 'Ange hela IP- eller IP-intervallen som ska uteslutas (en per rad) i rutan nedan. För att ange ett IP-intervall, använd jokertecknet "%".<br>Exempel: 74.6.1.2 eller 74.6.%';
+$lang['HIPE_admin_section1'] = 'IP-uteslutning';
+$lang['HIPE_admin_section2'] = 'Frågor på historiktabellen';
+$lang['HIPE_admin_section3'] = 'Resultatet av den historiska begäran';
+$lang['HIPE_description'] = 'Denna insticksmodul gör det möjligt att utesluta från historiken och statistiken för IP- eller IP-intervall. <br>Dess aktivering blockerar registrering i tabellen över IP *_historia som anges i tabellen nedan.';
+$lang['HIPE_hist_cleaned'] = 'Rensning av historiktabellen utförd.';
+$lang['HIPE_resquet_ok'] = 'Förfrågan OK.';
+$lang['HIPE_save_config'] = 'Konfiguration sparad.';

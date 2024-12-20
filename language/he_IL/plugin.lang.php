f31e17422bf1a00de482fb42224cb37db058da06
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:19:28 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Error_HIPE_BlacklistedIP'] = '!שגיאה. כתובת ה-IP שלך נחסמה. אינך יכול להירשם לגלריה זו. צור קשר עם המנהל לפרטים נוספים.';
+$lang['HIPE_CleanHist'] = 'ניקוי היסטוריה';
+$lang['HIPE_IPBlacklist_title'] = 'רשימת שחור להרשמה';
+$lang['HIPE_IPBlacklisted'] = 'מניעת הרשמה לגלריה של כתות ה-IP המוחלטות (רשימת שחור)';
+$lang['HIPE_IPByMember'] = 'כתות ה-IP לפי חברים';
+$lang['HIPE_IPByMember_description'] = 'הצגת הכתות המשמשות על ידי החברים, ממוינות לפי כתות ה-IP';
+$lang['HIPE_IPForMember'] = 'כתות ה-IP של חבר';
+$lang['HIPE_IPForMember_description'] = 'חיפוש והצגת הכתות המשויכות למשתמש רשום (ממוינות לפי כתות ה-IP)';
+$lang['HIPE_MemberForIp'] = 'חברים לכתות אחת';
+$lang['HIPE_MemberForIp_description'] = 'חיפוש והצגת משתמשים המשויכים לכתות אחת (ממוינים לפי שם)';
+$lang['HIPE_OnlyGuest'] = 'כתות ה-IP של אורחים בלבד';
+$lang['HIPE_OnlyGuest_description'] = 'הצגת הכתות המשמשות רק על ידי אורחים ומספר הפעמים שנמצאות בטבלת ההיסטוריה, ממוינות לפי מספר הפעמים';
+$lang['HIPE_admin_description1'] = 'הזן את ה-IP המלא או טווחי IP לאיסור (אחד בכל שורה) בתיבה למטה. כדי לציין טווח IP, השתמש בתו הכללי "%".<br>דוגמה: 74.6.1.2 או 74.6.%';
+$lang['HIPE_admin_section1'] = 'איסור IP';
+$lang['HIPE_admin_section2'] = 'שאילתות על טבלת ההיסטוריה';
+$lang['HIPE_admin_section3'] = 'תוצאת השאילתה ההיסטורית';
+$lang['HIPE_description'] = 'התוסף מאפשר לאסור מההיסטוריה והסטטיסטיקות כתות IP או טווחי IP.<br>הפעלתו חוסמת את הרשומה בטבלת ההיסטוריה שנקבעה בטבלה למטה.';
+$lang['HIPE_hist_cleaned'] = 'ניקוי טבלת ההיסטוריה בוצע.';
+$lang['HIPE_resquet_ok'] = 'בקשה אושרה.';
+$lang['HIPE_save_config'] = 'הגדרות נשמרו.';
+$lang['HIPE_version'] = '- גרסה:';
+$lang['IP_geolocalisation'] = 'גיאולוקציה';
+$lang['submit'] = 'שלח';

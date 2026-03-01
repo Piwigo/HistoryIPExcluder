efae5ddb16e7666ac22ca35ea6d70a1105e7e8f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-01 05:35:31 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['HIPE_version'] = ' - 버전:';
+$lang['IP_geolocalisation'] = '지리적 위치 정보';
+$lang['submit'] = '제출';
+$lang['Error_HIPE_BlacklistedIP'] = '오류! 귀하의 IP가 차단되었습니다. 이 갤러리에 가입할 수 없습니다. 자세한 내용은 관리자에게 문의하십시오.';
+$lang['HIPE_CleanHist'] = '기록 삭제';
+$lang['HIPE_IPBlacklist_title'] = '등록 블랙리스트';
+$lang['HIPE_IPBlacklisted'] = '차단된 IP(블랙리스트)의 갤러리 등록을 방지합니다';
+$lang['HIPE_IPByMember'] = '회원별 IP';
+$lang['HIPE_IPByMember_description'] = '회원이 사용한 IP를 IP별로 정렬하여 표시합니다';
+$lang['HIPE_IPForMember'] = '회원별 IP';
+$lang['HIPE_IPForMember_description'] = '등록된 사용자와 연결된 IP를 검색하여 표시합니다(IP별 정렬)';
+$lang['HIPE_MemberForIp'] = 'IP별 회원';
+$lang['HIPE_MemberForIp_description'] = '하나의 IP에 연결된 사용자를 검색하여 표시합니다(이름별 정렬)';
+$lang['HIPE_OnlyGuest'] = '게스트 전용 IP';
+$lang['HIPE_OnlyGuest_description'] = '게스트 전용 IP와 기록 테이블 내 발생 횟수(횟수 순 정렬) 표시';
+$lang['HIPE_admin_description1'] = '아래 입력란에 제외할 전체 IP 또는 IP 범위(라인당 하나씩)를 입력하세요. IP 범위를 지정하려면 와일드카드 문자 "%"를 사용하세요.<br>예시 : 74.6.1.2 또는 74.6.%';
+$lang['HIPE_admin_section1'] = 'IP 제외';
+$lang['HIPE_admin_section2'] = '기록 테이블 쿼리';
+$lang['HIPE_admin_section3'] = '과거 요청 결과';
+$lang['HIPE_description'] = '이 플러그인은 IP 또는 IP 범위를 기록 및 통계에서 제외할 수 있게 합니다.<br>활성화 시 아래 테이블에 지정된 IP *_history 테이블의 기록이 차단됩니다.';
+$lang['HIPE_hist_cleaned'] = '기록 테이블 정리 완료.';
+$lang['HIPE_resquet_ok'] = '요청 성공.';
+$lang['HIPE_save_config'] = '설정 저장됨.';
